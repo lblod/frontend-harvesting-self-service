cf7938a720967ca7db609e01b1a12d11bd68d5ea
achraf <achrafatauil@hotmail.com>
2020-11-19 15:39:46 +0100

    add sort, page, size for overview table

---
+++ app/controllers/harvest/index.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+export default class HarvestIndexController extends Controller {
+  @tracked page = 0;
+  @tracked sort = 'created';
+  @tracked size = 15;
+
+}
