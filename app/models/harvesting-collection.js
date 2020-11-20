9c4c196f29d67d13820fd86718ff4ed40e464d81
achraf <achrafatauil@hotmail.com>
2020-11-20 15:53:43 +0100

    fix hrvst-collection model

--- app/models/harvesting-collection.js
+++ app/models/harvesting-collection.js
@@ -1,7 +1,6 @@
-import Model, { attr, hasMany } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class HarvestingCollectionModel extends Model {
   @attr('string') status;
-  @attr ('date') remoteDataObject;
-  @hasMany('harvesting-task') harvestingTasks;
+  @belongsTo('remote-data-object') remoteDataObject;
 }
