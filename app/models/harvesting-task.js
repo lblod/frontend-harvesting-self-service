182a1d7c9b03c4f304d4e8c0fe65be63170ca131
achraf <achrafatauil@hotmail.com>
2020-12-08 16:42:41 +0100

    rename generated relation to harvesting-collection + add shortCreator

--- app/models/harvesting-task.js
+++ app/models/harvesting-task.js
@@ -1,15 +1,20 @@
 import Model, { attr, belongsTo }  from '@ember-data/model';
 
 export default class HarvestingTaskModel extends Model {
-    @attr('string') status;
-    @attr('date') created;
-    @attr('date') modified;
-    @attr('string') creator;
-    @attr('string') graph;
-    @belongsTo('harvesting-collection') generated;
+  @attr('string') status;
+  @attr('date') created;
+  @attr('date') modified;
+  @attr('string') creator;
+  @attr('string') graph;
+  @belongsTo('harvesting-collection') harvestingCollection;
 
-    get shortStatus(){
-      const split = this.status.split('/')
-      return split[split.length-1]
-    }
+  get shortStatus(){
+    const split = this.status.split('/')
+    return split[split.length-1]
+  }
+
+  get shortCreator(){
+    const split = this.creator.split('/')
+    return split[split.length-1]
+  }
 } 
