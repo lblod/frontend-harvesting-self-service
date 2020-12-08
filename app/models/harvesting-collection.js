b242da8346bfe507ae3f2a2477622a66c09eb167
achraf <achrafatauil@hotmail.com>
2020-12-08 16:41:34 +0100

    rename has-part relationship to remoteDataObject

--- app/models/harvesting-collection.js
+++ app/models/harvesting-collection.js
@@ -2,5 +2,5 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class HarvestingCollectionModel extends Model {
   @attr('string') creator
-  @hasMany('remote-data-object') hasPart;
+  @hasMany('remote-data-object') remoteDataObjects;
 }
