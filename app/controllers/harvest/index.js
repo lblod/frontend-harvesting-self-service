b645b32fdffccea894969d5a8f31bac13563582c
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-12-18 14:38:24 +0100

    use ember-concurrency

--- app/controllers/harvest/index.js
+++ app/controllers/harvest/index.js
@@ -1,25 +1,24 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-
+import { task } from 'ember-concurrency-decorators';
 export default class HarvestIndexController extends Controller {
   @tracked page = 0;
   @tracked sort = 'created';
   @tracked size = 15;
 
-  @action
-  async deleteTask(task){
-    const collection = await task.harvestingCollection;
+  @task
+  *deleteHarvestingTask(task){
+    const collection = yield task.harvestingCollection;
     if(collection && collection.id){
-      const remoteDataObjects = await collection.remoteDataObjects;
+      const remoteDataObjects = yield collection.remoteDataObjects;
 
       //Delete top level down the tree, less confusing if something fails.
-      await task.destroyRecord();
-      await collection.destroyRecord();
+      yield task.destroyRecord();
+      yield collection.destroyRecord();
       for(const rObj of remoteDataObjects.toArray()){
-        await rObj.destroyRecord();
+        yield rObj.destroyRecord();
       }
     }
-    alert('deleted');
   }
 }
