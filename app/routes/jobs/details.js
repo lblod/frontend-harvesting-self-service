05e1baa6c29cd159be750a113afbcec67ea8740d
achraf <achrafatauil@hotmail.com>
2020-12-28 11:46:55 +0100

    change job.details route to lok for job instead of harvesting-task

--- app/routes/jobs/details.js
+++ app/routes/jobs/details.js
@@ -3,6 +3,6 @@ import Route from '@ember/routing/route';
 export default class JobDetailsRoute extends Route {
 
   model(param){
-    return this.store.findRecord('harvesting-task', param.id);
+    return this.store.findRecord('job', param.id);
   }
 }
