8b0aebd5ec2e4e7c624b2bbb5c3623cc11541455
achraf <achrafatauil@hotmail.com>
2020-12-29 11:44:51 +0100

    Add task datatable for files

---
+++ app/controllers/jobs/task/index.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+
+export default class JobsTaskIndexController extends Controller {
+  sort = "created";
+  page = 0;
+  size = 15;
+
+}
--- app/routes/jobs/task/index.js
+++ app/routes/jobs/task/index.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default class JobsTaskIndexRoute extends Route {
+export default class JobsTaskIndexRoute extends Route.extend(DataTableRouteMixin) {
+  modelName = 'remote-data-object';
+
+  async beforeModel(){
+    const container = await this.modelFor('jobs.task').inputContainers;
+    this.collection = container.firstObject.harvestingCollections
+  };
+
+  mergeQueryOptions() {
+    return {
+      include: 'harvesting-collection',
+      'filter[harvesting-collection][:id:]': this.collection.id,
+      sort: '-created'
+    };
+  }
 }
