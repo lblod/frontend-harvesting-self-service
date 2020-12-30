27ebd5bd21866c5838155b46cc53934b042da572
achraf <achrafatauil@hotmail.com>
2020-12-30 10:51:59 +0100

    look for filtered files instead of remote-data-objects

--- app/routes/jobs/task/index.js
+++ app/routes/jobs/task/index.js
@@ -2,22 +2,18 @@ import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
 export default class JobsTaskIndexRoute extends Route.extend(DataTableRouteMixin) {
-  modelName = 'remote-data-object';
+  modelName = 'file';
 
   // NOTE: this should be changed later when model is definite. Now just take the first containers & first harvestingCollection
 
   async beforeModel(){
-    let container = await this.modelFor('jobs.task').inputContainers;
-    let firstContainer = await container.firstObject;
-    let collection = await firstContainer.harvestingCollections;
-    let firstCollection = await collection.firstObject;
-    this.collectionId = firstCollection.id;
+    this.taskId = await this.modelFor('jobs.task').id;
   }
 
   mergeQueryOptions() {
     return {
-      include: 'harvesting-collection',
-      'filter[harvesting-collection][:id:]': this.collectionId,
+      include: 'data-container',
+      'filter[data-container][result-from-tasks][:id:]': this.taskId,
       sort: '-created'
     };
   }
