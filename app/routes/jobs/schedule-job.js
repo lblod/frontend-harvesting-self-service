b82ea9ab8acf2b45a77f8b79edf496400fa706e3
achraf <achrafatauil@hotmail.com>
2020-12-30 16:41:11 +0100

    add setupController reseting job creating properties

--- app/routes/jobs/schedule-job.js
+++ app/routes/jobs/schedule-job.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default class JobsScheduleJobRoute extends Route {
+
+  setupController( controller, model ) {
+    super.setupController(...arguments);
+    controller.set('selectedJobOperation', null);
+    controller.set('success', false);
+    controller.set('error', false);
+    controller.set('url', null);
+    controller.set('graphName', null);
+  }
 }
