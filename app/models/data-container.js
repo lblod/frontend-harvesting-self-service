68b2a8d965025ff90170efa0f88c8281c4a81e14
achraf <achrafatauil@hotmail.com>
2020-12-30 14:44:07 +0100

    add hasGraph attr to data-container model

--- app/models/data-container.js
+++ app/models/data-container.js
@@ -2,6 +2,7 @@ import Model, { hasMany, attr }  from '@ember-data/model';
 
 export default class DataContainerModel extends Model {
   @attr('string') uri;
+  @attr('string') hasGraph;
   @hasMany('file') files;
   @hasMany('harvesting-collection') harvestingCollections;
 }
