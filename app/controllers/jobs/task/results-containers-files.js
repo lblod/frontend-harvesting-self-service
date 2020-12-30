5684dc945fe804a8b4361fce2e52bf5c0577bcd0
achraf <achrafatauil@hotmail.com>
2020-12-30 15:49:43 +0100

    change created sorting to show more recent first

--- app/controllers/jobs/task/results-containers-files.js
+++ app/controllers/jobs/task/results-containers-files.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class JobsTaskResultController extends Controller {
-  sort = "created";
+  sort = "-created";
   page = 0
   size = 15;
 
