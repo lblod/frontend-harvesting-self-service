1eef461e559b3764abb5c2143eba4fa8fb8f6ee5
achraf <achrafatauil@hotmail.com>
2020-12-30 11:59:11 +0100

    add input & result controllers

--- app/controllers/jobs/task/result.js
+++ app/controllers/jobs/task/result.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
-export default class JobsTaskIndexController extends Controller {
+export default class JobsTaskResultController extends Controller {
   sort = "created";
   page = 0
   size = 15;
