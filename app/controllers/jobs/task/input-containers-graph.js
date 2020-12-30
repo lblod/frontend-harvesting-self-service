c6b47c5c090cf15bb555c1d261f62f879a729127
achraf <achrafatauil@hotmail.com>
2020-12-30 15:50:16 +0100

    add data table attr (sort, page, size) to new task subroutes

--- app/controllers/jobs/task/input-containers-graph.js
+++ app/controllers/jobs/task/input-containers-graph.js
@@ -1,4 +1,8 @@
 import Controller from '@ember/controller';
 
 export default class JobsTaskInputContainersGraphController extends Controller {
+  sort = "";
+  page = 0
+  size = 15;
+
 }
--- app/controllers/jobs/task/results-containers-graph.js
+++ app/controllers/jobs/task/results-containers-graph.js
@@ -1,4 +1,8 @@
 import Controller from '@ember/controller';
 
 export default class JobsTaskResultsContainersGraphController extends Controller {
+  sort = "";
+  page = 0
+  size = 15;
+
 }
