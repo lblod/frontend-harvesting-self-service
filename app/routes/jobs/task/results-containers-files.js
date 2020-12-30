1cdae56c2f8a8a0350d1cecfb4845f729e8845e5
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-12-30 13:17:24 +0100

    added results container

--- app/routes/jobs/task/results-containers-files.js
+++ app/routes/jobs/task/results-containers-files.js
@@ -4,15 +4,12 @@ import DataTableRouteMixin from 'ember-data-table/mixins/route';
 export default class JobsTaskResultRoute extends Route.extend(DataTableRouteMixin) {
   modelName = 'file';
 
-  // NOTE: this should be changed later when model is definite. Now just take the first containers & first harvestingCollection
-
   async beforeModel(){
     this.taskId = await this.modelFor('jobs.task').id;
   }
 
   mergeQueryOptions() {
     return {
-      include: 'data-container',
       'filter[data-container][result-from-tasks][:id:]': this.taskId,
       sort: '-created'
     };
