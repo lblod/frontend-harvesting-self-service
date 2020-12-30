f682bcef7d5d7563702fe11040192588f827b1ce
achraf <achrafatauil@hotmail.com>
2020-12-30 15:22:43 +0100

    Upgrade jobs/details/index to octane syntax

--- app/routes/jobs/details/index.js
+++ app/routes/jobs/details/index.js
@@ -1,12 +1,12 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Ember.Route.extend(DataTableRouteMixin, {
-  modelName: 'task',
+export default class JobsDetailsIndexRoute extends Route.extend(DataTableRouteMixin) {
+  modelName = 'task';
 
   async beforeModel(){
     this.job = await this.modelFor('jobs.details').id;
-  },
+  }
 
   mergeQueryOptions(param) {
     return {
@@ -15,4 +15,4 @@ export default Ember.Route.extend(DataTableRouteMixin, {
       sort: param.sort
     };
   }
-});
+}
