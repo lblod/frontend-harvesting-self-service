8f204c6f34f72ca7b9397b2bce68696de5867ec1
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-12-30 12:30:33 +0100

    added column with size

--- app/models/file.js
+++ app/models/file.js
@@ -1,6 +1,7 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class FileModel extends Model {
+  @attr('string') uri;
   @attr('string') name;
   @attr('string') format;
   @attr('number') size;
@@ -8,4 +9,17 @@ export default class FileModel extends Model {
   @attr('date') created;
   @belongsTo('remote-data-object') remoteDataObject;
   @belongsTo('data-container') dataContainer;
+
+  get humanReadableSize(){
+    //ripped from https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+    const bytes = this.size;
+    const sizes = ['bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 byte';
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+  }
+
+  get downloadLink(){
+    return `/files/${this.id}/download?name=${this.name}`;
+  };
 }
