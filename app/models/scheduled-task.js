f0a29ab56aac2417441e4e213185c70b7cf45659
Unknown <achrafatauil@hotmail.com>
2021-09-08 16:37:45 +0200

    add new schedule models

---
+++ app/models/scheduled-task.js
@@ -0,0 +1,14 @@
+import Model, { attr, belongsTo, hasMany }  from '@ember-data/model';
+
+export default class ScheduledTaskModel extends Model {
+  @attr('string') uri;
+  @attr('date') created;
+  @attr('date') modified;
+  @attr('string') operation;
+  @attr('string') index;
+
+  @belongsTo('scheduled-job') scheduledJob;
+
+  @hasMany('task') parentTasks;
+  @hasMany('data-container') inputContainers;
+}
