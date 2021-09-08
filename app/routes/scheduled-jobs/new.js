12bc3dc7da3d733538c861414073255dee59bbb0
Unknown <achrafatauil@hotmail.com>
2021-09-08 16:41:22 +0200

    reset properties of new scheduledJob page

--- app/routes/scheduled-jobs/new.js
+++ app/routes/scheduled-jobs/new.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default class ScheduledJobsNewRoute extends Route {
+  setupController( controller, model ) {
+    super.setupController(...arguments);
+    controller.set('selectedJobOperation', null);
+    controller.set('success', false);
+    controller.set('error', false);
+    controller.set('url', null);
+    controller.set('title', '');
+    controller.set('cronPattern', '*/5 * * * *');
+  }
 }
