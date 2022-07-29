7e2b33cee5dc532e0f39d8d4498bb3d81acd809d
iurianu <iulianandriescu@gmail.com>
2022-07-29 16:59:35 +0300

    Add new operation to list

--- app/components/job-operation.js
+++ app/components/job-operation.js
@@ -21,6 +21,8 @@ export default class JobOperationComponent extends Component {
         const firstTask = tasks.find((task) => task.get('index') === '0');
         const job = yield this.args.job;
         if (firstTask) {
+
+          console.log('operation: ', job.operation)
             
           if (job.operation === 'http://lblod.data.gift/id/jobs/concept/JobOperation/lblodHarvestAndPublish') {
             this.operation = 'Harvest & Publish'
@@ -28,6 +30,8 @@ export default class JobOperationComponent extends Component {
             this.operation = 'Harvest'
           } else if (job.operation === 'http://redpencil.data.gift/id/jobs/concept/JobOperation/deltas/healingOperation/besluiten') {
             this.operation = 'Harvested'
+          } else if (job.operation === 'http://redpencil.data.gift/id/jobs/concept/JobOperation/deltas/deltaDumpFileCreation/besluiten') {  
+            this.operation = 'Dumped File'
           } else {
             this.operation = 'Publish'
           }
