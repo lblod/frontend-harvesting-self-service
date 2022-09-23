1da933f771a31095a6f83e561cb3d5f2b71361b5
iurianu <iulianandriescu@gmail.com>
2022-09-23 18:36:35 +0300

    clean code, and fix typo on jobs/index

--- app/routes/jobs/index.js
+++ app/routes/jobs/index.js
@@ -7,13 +7,13 @@ export default class JobsIndexRoute extends Route.extend(DataTableRouteMixin) {
     page: { refreshModel: true, replace: true },
     size: { refreshModel: true },
     sort: { refreshModel: true },
-    status: { status: this.shortStatus, refreshModel: true },
+    status: { refreshModel: true },
   };
 
   mergeQueryOptions(param) {
     const options = {
       sort: param.sort,
-      page: { numer: param.number, size: param.size },
+      page: { number: param.number, size: param.size },
       include: 'tasks',
     };
 
