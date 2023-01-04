2a4217401b7eeb4fcb6527bd7ed2357c7268aefa
felix <ruiz.felixruiz@gmail.com>
2023-01-04 15:59:22 +0100

    If no label found just display URI

--- app/helpers/operation-name.js
+++ app/helpers/operation-name.js
@@ -3,5 +3,5 @@ import { JOB_OP_TYPES } from '../utils/constants';
 
 export default helper(function operationName([uri] /*, hash*/) {
   const name = JOB_OP_TYPES.get(uri);
-  return name || '';
+  return name || uri;
 });
