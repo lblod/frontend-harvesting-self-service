fa804797150aa7b812508fac4a00fae23ab6b61a
Achraf Atauil <achrafatauil@hotmail.com>
2023-03-06 15:07:10 +0100

    lint fix

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
   @service session;
-  
+
   authenticationEnabled = ['true', 'True', 'TRUE', true].includes(
     config.harvester.authEnabled
   );
