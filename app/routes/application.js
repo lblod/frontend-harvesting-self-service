24c88091e464493089b9fb3501792add7ddbe6bb
benjay10 <ben.behaeghe@redpencil.io>
2023-03-17 17:33:37 +0100

    Fix typos

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
-
   @service session;
 
   async beforeModel() {
