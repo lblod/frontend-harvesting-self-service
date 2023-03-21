157ddbf55aa521fca00048ba27d8536292e32986
benjay10 <ben.behaeghe@redpencil.io>
2023-03-21 01:12:34 +0100

    Fix missing store

--- app/routes/jobs/task.js
+++ app/routes/jobs/task.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class JobsTaskRoute extends Route {
+  @service store;
+
   model(param) {
     return this.store.findRecord('task', param.id);
   }
