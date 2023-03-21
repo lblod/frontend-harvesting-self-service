774104662c366ee5fa2f8cbd0a878cb3eb1f350e
benjay10 <ben.behaeghe@redpencil.io>
2023-03-21 20:44:51 +0100

    Fix explicit polymorphic relations

--- app/models/authentication-configuration.js
+++ app/models/authentication-configuration.js
@@ -2,6 +2,12 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class AuthenticationConfiguration extends Model {
   @attr uri;
-  @belongsTo('credential', { async: true, inverse: null }) credential;
-  @belongsTo('security-scheme', { async: true, inverse: null }) securityScheme;
+  @belongsTo('credential', { async: true, inverse: null, polymorphic: true })
+  credential;
+  @belongsTo('security-scheme', {
+    async: true,
+    inverse: null,
+    polymorphic: true,
+  })
+  securityScheme;
 }
