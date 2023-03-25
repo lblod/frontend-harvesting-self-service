fab05fb95155eee08f490f473776485067d6fdad
benjay10 <ben.behaeghe@redpencil.io>
2023-03-25 04:26:10 +0100

    Implement authentication protection on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,18 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import config from 'frontend-harvesting-self-service/config/environment';
 
 export default class IndexRoute extends Route {
-  beforeModel() {
+  @service session;
+
+  beforeModel(transition) {
+    const authenticationEnabled = ['true', 'True', 'TRUE', true].includes(
+      config.harvester.authEnabled
+    );
+
+    if (authenticationEnabled) {
+      this.session.requireAuthentication(transition, 'login');
+    }
     this.transitionTo('overview.jobs');
   }
 }
