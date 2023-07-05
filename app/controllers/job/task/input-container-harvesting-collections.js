39ef4ad1eb0fd491a49ec3ab966d71ba9ac1e632
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2023-07-05 15:18:25 +0200

    fix: solve pagination on the Input Container Harvesting Collections tab

--- app/controllers/job/task/input-container-harvesting-collections.js
+++ app/controllers/job/task/input-container-harvesting-collections.js
@@ -2,6 +2,9 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class JobTaskInputContainerHarvestingCollectionsController extends Controller {
+  sort = '';
+  page = 0;
+  size = 15;
   @action
   overrideClick(e) {
     e.stopPropagation();
