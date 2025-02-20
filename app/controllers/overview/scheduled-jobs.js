1cdf20992ecaf96b0a62b9d31bfde9a1e345529f
Andres Vergauwen <andres.vergauwen@hotmail.com>
2025-02-20 13:43:06 +0100

    Change size back to 15

--- app/controllers/overview/scheduled-jobs.js
+++ app/controllers/overview/scheduled-jobs.js
@@ -3,6 +3,6 @@ import Controller from '@ember/controller';
 export default class OverviewScheduledJobsController extends Controller {
   page = 0;
   sort = '-created';
-  size = 20;
+  size = 15;
   search = '';
 }
