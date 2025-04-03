61c8b0e69fba6b549311d736b48a9a5d4ef458c9
Niels Vandekeybus <progster@gmail.com>
2025-04-03 10:15:44 +0200

    ember upgrade v4.8.0...v4.12.3

--- config/environment.js
+++ config/environment.js
@@ -17,6 +17,7 @@ module.exports = function (environment) {
       worshipHarvesting: '{{WORSHIP_HARVESTING_ENABLED}}',
     },
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
