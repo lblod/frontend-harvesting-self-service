45a5fe976ca525996a89bf4c9fe95201121e45e6
Niels V <niels.vandekeybus@redpencil.io>
2025-04-03 19:44:17 +0200

    modernize login screen

--- app/components/mu-login.js
+++ app/components/mu-login.js
@@ -1,3 +1,11 @@
 import BaseMuLoginComponent from 'ember-mu-login/components/mu-login';
 
-export default class MuLoginComponent extends BaseMuLoginComponent {}
+export default class MuLoginComponent extends BaseMuLoginComponent {
+  handleNicknameInput = (event) => {
+    this.nickname = event.target.value.trim();
+  };
+
+  handlePasswordInput = (event) => {
+    this.password = event.target.value.trim();
+  };
+}
