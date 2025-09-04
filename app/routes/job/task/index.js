5dcbabbc78ad84a08319828055dd06dccb6fda92
Niels Vandekeybus <progster@gmail.com>
2025-09-04 15:45:55 +0200

    Add conditional tab display for collecting tasks
    Show only harvesting collection tab for collecting operations while maintaining all tabs for other task types. Added index route to automatically redirect to appropriate default tab.

---
+++ app/routes/job/task/index.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+
+export default class JobTaskIndexRoute extends Route {
+  afterModel(model, transition) {
+    const task = this.modelFor('job.task');
+    
+    if (task.operation === 'http://lblod.data.gift/id/jobs/concept/TaskOperation/collecting') {
+      this.transitionTo('job.task.input-container-harvesting-collections');
+    } else {
+      this.transitionTo('job.task.input-container-files');
+    }
+  }
+}
