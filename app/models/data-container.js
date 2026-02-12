62e5b527692515aab38726f9c382c554b56329f7
karel kremer <karel.kremer@redpencil.io>
2026-02-12 16:47:42 +0100

    show more containers

--- app/models/data-container.js
+++ app/models/data-container.js
@@ -3,6 +3,7 @@ import Model, { hasMany, attr } from '@ember-data/model';
 export default class DataContainerModel extends Model {
   @attr uri;
   @attr hasGraph;
+  @attr hasResource;
   @hasMany('file', { async: true, inverse: 'dataContainer' }) files;
   @hasMany('harvesting-collection', { async: true, inverse: null })
   harvestingCollections;
---
+++ app/routes/job/task/results-container-harvesting-collections.js
@@ -0,0 +1,23 @@
+import Route from '@ember/routing/route';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import { service } from '@ember/service';
+
+export default class JobTaskResultsContainerHarvestingCollectionsRoute extends Route.extend(
+  DataTableRouteMixin,
+) {
+  @service store;
+
+  modelName = 'remote-data-object';
+
+  async beforeModel() {
+    this.task = await this.modelFor('job.task');
+  }
+
+  mergeQueryOptions(param) {
+    return {
+      'filter[harvesting-collection][data-container][result-from-tasks][:id:]':
+        this.task.id,
+      sort: param.sort,
+    };
+  }
+}
