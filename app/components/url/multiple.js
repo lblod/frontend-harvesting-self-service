f9152d30c1ae44e6b62934388e1e80e50696149c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-03-27 11:14:58 +0100

    ifeat: create url::multiple component to add more urls values
    Refs: lbron-965

---
+++ app/components/url/multiple.js
@@ -0,0 +1,43 @@
+import Component from '@glimmer/component';
+
+import { A } from '@ember/array';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { v4 as uuid } from 'uuid';
+
+export default class UrlMultiple extends Component {
+  @tracked url;
+  @tracked otherUrls = A([]);
+
+  @action
+  updateUrl(urlValue) {
+    this.url = urlValue;
+    this.args.onChange?.(this.allUrls);
+  }
+
+  @action
+  updateOtherUrl(urlObject, newUrlValue) {
+    const selectedObjectIndex = this.otherUrls.indexOf(urlObject);
+    const newUrlObject = {
+      id: urlObject.id,
+      value: newUrlValue,
+    };
+    this.otherUrls.replace(selectedObjectIndex, 1, [newUrlObject]);
+    this.args.onChange?.(this.allUrls);
+  }
+
+  @action
+  addEmptyUrl() {
+    const newUrlObject = {
+      id: `${this.args.id}-${uuid()}`,
+      value: null,
+    };
+    this.otherUrls.pushObject(newUrlObject);
+  }
+
+  get allUrls() {
+    const othersAsValues = this.otherUrls.map((urlObject) => urlObject.value);
+
+    return [this.url, ...othersAsValues].filter((url) => url);
+  }
+}
