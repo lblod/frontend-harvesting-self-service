a7e417bc27058d8f006385a9cc4ab6a0d7369c73
brechtvdv <br5cht@hotmail.com>
2026-04-08 18:21:47 +0200

    Change decisionUrisValid by checking in loop. Fix isValid when single url

--- app/controllers/overview/jobs/new.js
+++ app/controllers/overview/jobs/new.js
@@ -250,6 +250,7 @@ export default class OverviewJobsNewController extends Controller {
       } else if (this.isJobWithSingleUrl) {
         sources.push(this.url.trim());
       } else if (this.isJobWithMultipleEndpoints) {
+        this.decisionUrisValid = true;
         const newLinePattern = /\r?\n/;
         this.url.split(newLinePattern).map((source) => {
           if (!isValidUrl(source)) {
