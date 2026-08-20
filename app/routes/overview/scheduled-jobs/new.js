75978cb834c8b71e268795a019653957ea810c5d
karel kremer <karel.kremer@redpencil.io>
2026-08-20 11:10:12 +0200

    don't lose new job setup when refreshing route

--- app/routes/overview/scheduled-jobs/new.js
+++ app/routes/overview/scheduled-jobs/new.js
@@ -1,22 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default class OverviewScheduledJobsNewRoute extends Route {
-  setupController(controller /*, model */) {
-    super.setupController(...arguments);
-    controller.set('title', null);
-    controller.set('titleValid', true);
-    controller.set('url', null);
-    controller.set('urlValid', true);
-    controller.set('selectedJobOperation', null);
-    controller.set('selectedJobOperationValid', true);
-    controller.set('selectedMunicipality', null);
-    controller.set('codelistUri', null);
-    controller.set('cronPattern', '*/5 * * * *');
-    controller.set('cronPatternValid', true);
-    controller.set('vendor', null);
-    controller.set('vendorValid', true);
-    controller.set('selectedSecurityScheme', null);
-    controller.set('securityScheme', {});
-    controller.set('credentials', {});
+  setupController(controller, _model, transition) {
+    if (transition?.from?.name != transition?.to?.name) {
+      super.setupController(...arguments);
+      controller.set('title', null);
+      controller.set('titleValid', true);
+      controller.set('url', null);
+      controller.set('urlValid', true);
+      controller.set('selectedJobOperation', null);
+      controller.set('selectedJobOperationValid', true);
+      controller.set('selectedMunicipality', null);
+      controller.set('codelistUri', null);
+      controller.set('cronPattern', '*/5 * * * *');
+      controller.set('cronPatternValid', true);
+      controller.set('vendor', null);
+      controller.set('vendorValid', true);
+      controller.set('selectedSecurityScheme', null);
+      controller.set('securityScheme', {});
+      controller.set('credentials', {});
+    }
   }
 }
